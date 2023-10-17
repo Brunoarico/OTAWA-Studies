dc8d8660c773c25e27f4db8c9459ff896cf64280
Gustavo Pereira <gugagop4@gmail.com>
2023-10-17 15:30:24 -0300

    Fix ACO errors

--- loop-cluster/ACO.cpp
+++ loop-cluster/ACO.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <cmath>
+#include <time.h>
 
 struct Ant {
     int *tour;
@@ -13,6 +14,7 @@ struct Ant {
 
 struct Colony {
     struct Ant *ant;
+    struct Ant queen;
     // ... other colony properties
 };
 
@@ -33,7 +35,7 @@ void updatePhromone(double **tau, struct Colony colony, int nodeNo, int antNo) {
 
 
 int rouletteWheel(double *P, int length) {
-    double *cumsumP = malloc(length * sizeof(double));
+    double *cumsumP = (double *) malloc(length * sizeof(double));
     cumsumP[0] = P[0];
 
     // Compute cumulative sum
@@ -69,7 +71,7 @@ int fitnessFunction(int *tour, int tourLength) {
     return fitness;
 }
 
-void createColony(int **tau, int **eta, int length_edges, struct Colony *colony, int antNo, double alpha, double beta) {
+void createColony(int length_edges, double** tau, double** eta, struct Colony *colony, int antNo, double alpha, double beta) {
     int nodeNo = length_edges;
 
     for (int i = 0; i < antNo; ++i) {
@@ -83,7 +85,7 @@ void createColony(int **tau, int **eta, int length_edges, struct Colony *colony,
         while (nextNode <= length_edges - 1) { // to choose the rest of nodes
             int currentNode = colony->ant[i].tour[nextNode];
 
-            double *P_allNodes = malloc(nodeNo * sizeof(double));
+            double *P_allNodes = (double *) malloc(nodeNo * sizeof(double));
             for (int j = 0; j < nodeNo; ++j) {
                 P_allNodes[j] = pow(tau[currentNode][j], alpha) * pow(eta[currentNode][j], beta);
             }
@@ -94,7 +96,7 @@ void createColony(int **tau, int **eta, int length_edges, struct Colony *colony,
                 sumP += P_allNodes[j];
             }
 
-            double *P = malloc(nodeNo * sizeof(double));
+            double *P = (double *) malloc(nodeNo * sizeof(double));
             for (int j = 0; j < nodeNo; ++j) {
                 P[j] = P_allNodes[j] / sumP;
             }
@@ -111,4 +113,125 @@ void createColony(int **tau, int **eta, int length_edges, struct Colony *colony,
             free(P);
         }
     }
+}
+
+double mean(int *array, int size) {
+    double sum = 0.0;
+    for (int i = 0; i < size; ++i) {
+        sum += array[i];
+    }
+    return sum / size;
+}
+
+int main()
+{
+    int graph[16][16] = {
+        {0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 1, 3, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 4, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 4, 2, 3, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+    };
+
+    int length_edges = sizeof(graph) / sizeof(graph[0]);
+
+        srand(time(NULL));
+
+    // Inicializa a matriz resultante
+    int matriz_resultante[length_edges];
+
+    // Gera um índice aleatório dentro do número de colunas para cada linha
+    for (int i = 0; i < length_edges; ++i) {
+        int indice_aleatorio = rand() % 16;  // Gera um índice aleatório de 0 a 15
+        matriz_resultante[i] = graph[i][indice_aleatorio]; // Seleciona o elemento correspondente ao índice aleatório
+    }
+
+    int maxIter = 30;
+    int antNo = 1;
+
+    double tau0 = 10 * 1 / (length_edges * mean(&graph[0][0], length_edges * length_edges)); 
+
+    // Phromone matrix initialization
+    double tau[length_edges][length_edges];
+    for (int i = 0; i < length_edges; ++i) {
+        for (int j = 0; j < length_edges; ++j) {
+            tau[i][j] = tau0;
+        }
+    }
+
+    // Desirability of each edge (eta)
+    double eta[length_edges][length_edges];
+    for (int i = 0; i < length_edges; ++i) {
+        for (int j = 0; j < length_edges; ++j) {
+            eta[i][j] = graph[i][j];
+        }
+    }
+
+    double rho = 0.5; // Evaporation rate 
+    double alpha = 1; // Phromone exponential parameters 
+    double beta = 1;  // Desirability exponential parameter 
+
+    double bestFitness = -INFINITY;
+    int bestTour[2] = {0}; // Assuming tourRow and tourColumn are integers
+
+    struct Colony colony;
+    double Wcet[maxIter];
+
+    for (int t = 0; t < maxIter; ++t) {
+        // Create Ants
+        createColony(length_edges, tau, eta, &colony, antNo, alpha, beta);
+
+        // Calculate the fitness values of all ants
+        for (int i = 0; i < antNo; ++i) {
+            colony.ant[i].fitness = fitnessFunction(colony.ant[i].wcet, length_edges);
+        }
+
+        // Find the best ant (queen)
+        double maxVal = -INFINITY;
+        int maxIndex = -1;
+        for (int i = 0; i < antNo; ++i) {
+            if (colony.ant[i].fitness > maxVal) {
+                maxVal = colony.ant[i].fitness;
+                maxIndex = i;
+            }
+        }
+
+        if (maxVal > bestFitness) {
+            bestFitness = maxVal;
+            bestTour[0] = *colony.ant[maxIndex].tourRow;
+            bestTour[1] = *colony.ant[maxIndex].tourColumn;
+        }
+
+        colony.queen.fitness = bestFitness;
+        // Assuming queen.tour is an array of size 2
+        colony.queen.tour[0] = bestTour[0];
+        colony.queen.tour[1] = bestTour[1];
+
+        // Update pheromone matrix
+        updatePhromone(tau, colony, length_edges, antNo);
+
+        // Evaporation
+        for (int i = 0; i < length_edges; ++i) {
+            for (int j = 0; j < length_edges; ++j) {
+                tau[i][j] *= (1 - rho);
+            }
+        }
+
+        // Display the results
+        printf("Iteration #%d: Longest length = %d\n", t + 1, colony.queen.fitness);
+        Wcet[t] = colony.queen.fitness;
+    }
+    
+    return 0;
 }
