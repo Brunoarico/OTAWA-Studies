6edcaf60c55dd037f39ebdcd4dba342b061667a6
Guilherme Melaninho <guilherme.melaninho@gmail.com>
2023-11-13 00:23:24 -0300

    Incluido novos testes

--- biowcet/tests/aux_functions.cpp
+++ biowcet/tests/aux_functions.cpp
@@ -2,41 +2,77 @@
 #include <string>
 #include <cstdio>
 #include "../src/utils/utilities.h"
+#include "../src/cfgGen/cfgGen.h"
 #define TO_FF "../din//xml2ff.py"
 #define BUILD_FOLDER "../din/build"
 #define DINAMIC_FOLDER "../din"
 #define MAKE "make"
+#define RESULT_FILE "/results.txt"
 
-void genff() {
+void genff()
+{
     printf("Smart Generating of ff file...");
-    std::string command = std::string(TO_FF) +" "+ std::string(BUILD_FOLDER);
-    FILE* in = popen(command.c_str(), "r");
+    std::string command = std::string(TO_FF) + " " + std::string(BUILD_FOLDER);
+    FILE *in = popen(command.c_str(), "r");
     if (!in)
         printf("Failed to execute command: ");
-    else {
+    else
+    {
         char buffer[128];
-        while (fgets(buffer, sizeof(buffer), in) != NULL) printf("%s", buffer);
+        while (fgets(buffer, sizeof(buffer), in) != NULL)
+            printf("%s", buffer);
         pclose(in);
     }
 }
 
-void compile() {
+void compile()
+{
     printInfo("Compiling...");
     if (directoryChange(DINAMIC_FOLDER))
         executeAndLog(MAKE);
-    else {
+    else
+    {
         printError("Error in directory change");
         exit(1);
     }
 }
 
-void clearDir() {
+void clearDir()
+{
     printInfo("Cleaning directory");
-    if (directoryChange(DINAMIC_FOLDER)) {
+    if (directoryChange(DINAMIC_FOLDER))
+    {
         std::string cmd = MAKE + std::string(" clean");
         executeAndLog(cmd);
-    } else
+    }
+    else
         exit(1);
+}
+
+void toFile(std::string dir, std::string entry, uint32_t wceti, uint32_t wcetb, uint32_t wcetd, bool dyn) {
+    std::string outname = dir + RESULT_FILE;
+    printInfo("Writing results to " + outname);
+    FILE* outfile = fopen(outname.c_str(), "w");
+
+
+    if(wceti >= 0) fprintf(outfile, "WCET_IPET[%s] = %d cycles\n", entry.c_str(), wceti);
+
+    if(wcetb >= 0) fprintf(outfile, "WCET_BIO[%s] = %d cycles\n", entry.c_str(), wcetb);
+
+    if(wcetd >= 0 && dyn) fprintf(outfile, "WCET_DYNAMIC[%s] = %d cycles\n", entry.c_str(), wcetd);
+
+    fclose(outfile);
+}
+
+void copyToDir(std::string source, std::string dest) {
+    char cwd[256];
+    getcwd(cwd, 256);
+    std::string newDir = cwd + std::string("/..");
+    if (chdir(newDir.c_str()) == 0) {
+        std::string cwd_str = std::string(cwd);
+        std::string cmd = CP + std::string(" '") + source + std::string("' '") + cwd_str + dest + std::string("'");
+        executeAndLog(cmd.c_str());
+    }
 }
 
 
--- biowcet/tests/tests.cpp
+++ biowcet/tests/tests.cpp
@@ -6,34 +6,32 @@
 #include "../src/wipet/wcet_ipet.h"
 #include "../src/wbio/wcet_bio.h"
 #include "../src/wdyn/wcet_dyn.h"
+#include "../src/aco/aco.h"
 
 #define DEBUG_FILE "../din/hwdebug.py"
 #define ELF_FOLDER "../din/build/main.elf"
 #define ELF_TEST "../din/"
 #define ELF_OTAWA_FOLDER "../din/build/main_otawa.elf"
 
-
-
 // test if files are compiled
-TEST(CompileTest, CompileTest)
-{
-   int result = false;
-   compile();
-   FILE *fa = fopen(ELF_TEST".merge.cm", "r");
-   FILE *fb = fopen(ELF_TEST".rec_status", "r");
-
-   FILE *fc = fopen(ELF_TEST"build/main_empty.ff", "r");
-   FILE *fd = fopen(ELF_TEST"build/main_otawa.elf", "r");
-   FILE *fe = fopen(ELF_TEST"build/main.bin", "r");
-   FILE *ff = fopen(ELF_TEST"build/main.bin.lst", "r");
-   FILE *fg = fopen(ELF_TEST"build/main.elf", "r");
-   FILE *fh = fopen(ELF_TEST"build/main.xml", "r");
-   if (fa && fb && fc && fd && fe && ff && fg && fh)
-   {
-     result = true;
-   }
-   ASSERT_TRUE(result);
-}
+// TEST(CompileTest, CompileTest)
+// {
+//    int result = false;
+//    compile();
+//    FILE *fa = fopen(ELF_TEST ".merge.cm", "r");
+//    FILE *fb = fopen(ELF_TEST ".rec_status", "r");
+//    FILE *fc = fopen(ELF_TEST "build/main_empty.ff", "r");
+//    FILE *fd = fopen(ELF_TEST "build/main_otawa.elf", "r");
+//    FILE *fe = fopen(ELF_TEST "build/main.bin", "r");
+//    FILE *ff = fopen(ELF_TEST "build/main.bin.lst", "r");
+//    FILE *fg = fopen(ELF_TEST "build/main.elf", "r");
+//    FILE *fh = fopen(ELF_TEST "build/main.xml", "r");
+//    if (fa && fb && fc && fd && fe && ff && fg && fh)
+//    {
+//       result = true;
+//    }
+//    ASSERT_TRUE(result);
+// }
 // Generate .ff and .elf files for OTAWA
 TEST(GenffTest, GenFF)
 {
@@ -42,12 +40,28 @@ TEST(GenffTest, GenFF)
    ASSERT_TRUE(f != NULL);
 }
 
-
-
+TEST(CopyDirTest, CopyDir)
+{
+   int result = false;
+   compile();
+   FILE *fa = fopen(ELF_TEST ".merge.cm", "r");
+   FILE *fb = fopen(ELF_TEST ".rec_status", "r");
+   FILE *fc = fopen(ELF_TEST "build/main_empty.ff", "r");
+   FILE *fd = fopen(ELF_TEST "build/main_otawa.elf", "r");
+   FILE *fe = fopen(ELF_TEST "build/main.bin", "r");
+   FILE *ff = fopen(ELF_TEST "build/main.bin.lst", "r");
+   FILE *fg = fopen(ELF_TEST "build/main.elf", "r");
+   FILE *fh = fopen(ELF_TEST "build/main.xml", "r");
+   if (fa && fb && fc && fd && fe && ff && fg && fh)
+   {
+      result = true;
+   }
+   ASSERT_TRUE(result);
+}
 
 /*
  * Verify if WCET IPET functionality is working
- * 
+ *
  */
 TEST(WcetIipetM3Test, WcetIpetcalc)
 {
@@ -59,10 +73,9 @@ TEST(WcetIipetM3Test, WcetIpetcalc)
    ASSERT_TRUE(wcet_it);
 }
 
-
 /*
  * Verify if WCET Bio functionality is working
- * 
+ *
  */
 
 TEST(WcetBioM3Test, WcetBiocalc)
@@ -77,39 +90,83 @@ TEST(WcetBioM3Test, WcetBiocalc)
    ASSERT_TRUE(wcet_b);
 }
 
-// TEST(ACOCalculate, ACOCalculate)
-// {
-//    otawa::WorkSpace *ws;
-//    uint32_t wcet_b = -1;
-//    cfgGen otawaInstance("m3", "main", ELF_OTAWA_FOLDER);
-//    otawaInstance.workspaceGenerator();
-//    WCETCalculatorBio wcetBio(otawaInstance.cfg2Matrix());
-//    std::set<CfgMatrix> mySet = otawaInstance.cfg2Matrix();
-//     for (const CfgMatrix& elem : mySet) pq.push(elem);
-    
-//     while (!pq.empty()) {
-//         CfgMatrix c = pq.front();
-//         pq.pop();
-        
-//         int maxIter = 30;
-//         int antNo = 10;
-//         float rho = 0.8;
-//         if (c.getPriority() > 0 && !replaceDependencies(&c)) {
-//             pq.push(c);
-//         } else {
-//             c.printCycles();
-//             ACO aco(c, antNo, 0, maxIter, alpha, beta, rho);
-//             aco.simulate();
-//             wcet = aco.getResults();
-//             cfgMap[c.getMyHashName()] = wcet;
-//         }
-//     }
-//    ASSERT_TRUE(wcet_b);
-// }
+/*
+ * Verify if WCET Bio functionality is working
+ *
+ */
+TEST(ACOSimulateTest, ACOSimulate)
+{
+   otawa::WorkSpace *ws;
+   std::queue<CfgMatrix> pq;
+   uint32_t wcet = -1;
+   double alpha = 1; // Phromone exponential parameters
+   double beta = 1;  // Desirability exponential parameter
+   int maxIter = 30;
+   int antNo = 10;
+   float rho = 0.8;
+   cfgGen otawaInstance("m3", "main", ELF_OTAWA_FOLDER);
+   otawaInstance.workspaceGenerator();
+   std::set<CfgMatrix> mySet = otawaInstance.cfg2Matrix();
+   for (const CfgMatrix &elem : mySet)
+      pq.push(elem);
+
+   while (!pq.empty())
+   {
+      CfgMatrix c = pq.front();
+      pq.pop();
+
+      int maxIter = 30;
+      int antNo = 10;
+      float rho = 0.8;
+
+      ACO aco(c, antNo, 0, maxIter, alpha, beta, rho);
+      aco.simulate();
+      wcet = aco.getResults();
+       std::cout << wcet << "\n";
+      //cfgMap[c.getMyHashName()] = wcet;
+   }
+   ASSERT_TRUE(wcet);
+}
+
+TEST(CFGPrintCyclesTest, CFGPrintCycles)
+{
+  otawa::WorkSpace *ws;
+   std::queue<CfgMatrix> pq;
+   uint32_t wcet = -1;
+   double alpha = 1; // Phromone exponential parameters
+   double beta = 1;  // Desirability exponential parameter
+   int maxIter = 30;
+   int antNo = 10;
+   float rho = 0.8;
+   setVerbose(true);
+   cfgGen otawaInstance("m3", "main", ELF_OTAWA_FOLDER);
+   otawaInstance.workspaceGenerator();
+   std::set<CfgMatrix> mySet = otawaInstance.cfg2Matrix();
+   for (const CfgMatrix &elem : mySet)
+      pq.push(elem);
+
+   while (!pq.empty())
+   {
+      CfgMatrix c = pq.front();
+      pq.pop();
+
+      int maxIter = 30;
+      int antNo = 10;
+      float rho = 0.8;
+      c.printCycles();
+      ACO aco(c, antNo, 0, maxIter, alpha, beta, rho);
+      aco.simulate();
+      wcet = aco.getResults();
+      //cfgMap[c.getMyHashName()] = wcet;
+   }
+   ASSERT_TRUE(wcet);
+}
+
+
 
 /*
  * Verify if OTAWA workspace generator is working
- * 
+ *
  */
 TEST(WorkSpaceGeneratorTest, WorkSpace)
 {
@@ -119,10 +176,9 @@ TEST(WorkSpaceGeneratorTest, WorkSpace)
    ASSERT_TRUE(otawaInstance.workspaceGenerator());
 }
 
-
 /*
  * Verify if cfg2Matrix function returns a cfgmatrix data type
- * 
+ *
  */
 TEST(CfgToMatrixTest, CfgMatrixTest)
 {
@@ -130,10 +186,23 @@ TEST(CfgToMatrixTest, CfgMatrixTest)
    cfgGen otawaInstance("m3", "main", ELF_OTAWA_FOLDER);
    otawaInstance.workspaceGenerator();
    std::set<CfgMatrix> mySet = otawaInstance.cfg2Matrix();
-   for (const auto& cfgMatrix : mySet) {
-      ASSERT_TRUE(dynamic_cast<const CfgMatrix*>(&cfgMatrix) != nullptr);
+   for (const auto &cfgMatrix : mySet)
+   {
+      ASSERT_TRUE(dynamic_cast<const CfgMatrix *>(&cfgMatrix) != nullptr);
    }
-  
+}
+
+TEST(WriteOutputTest, WriteOutput)
+{
+   int result = false;
+   uint32_t wceti = 40;
+   uint32_t wcetb = 43; 
+   uint32_t wcetd = 44;
+   bool dyn = true;
+   toFile("/home/OTAWA-Studies/biowcet/tests", "main", wceti, wcetb, wcetd, dyn);
+   FILE *f = fopen("/home/OTAWA-Studies/biowcet/tests/results.txt", "r");
+   ASSERT_TRUE(f != NULL);
+
 }
 
 TEST(WcetDynamicTest, WcetDynamicCalc)
