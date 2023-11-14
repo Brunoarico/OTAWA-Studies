5f3b4acc6d2a6b97ea6cf72a9049f8869ec0b809
Brunoarico <brunoarico@gmail.com>
2023-11-14 11:18:29 -0300

    removing trashes

--- plotter/rho/wcets200/replay.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for arquivo in *.txt; do
-  linha=$(tail -n 1 "$arquivo")
-  echo "$linha"
-  for ((i=$(wc -l < "$arquivo"); i<200; i++)); do
-    echo "$linha" >> "$arquivo"
-  done
-done
